01d4827910ffdcc12dfb63a4671232b7146dd1c5
Muhammad Ali Khan Sajid <alikhansajid1181@gmail.com>
2025-01-19 00:21:52 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base:"/www.astra-zen.com/",
+   base:"/react-host/",
   optimizeDeps: {
     exclude: ['lucide-react'],
    
